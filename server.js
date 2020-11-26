1e80f3828cf98572386102c7bb16654e7d7cf67e
MacSantaCruz <santacrm@oregonstate.edu>
2020-11-26 11:25:57 -0800

    Added ENV variables and connect to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const express = require ('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const router = require('./routes/index');
 
-const app = express();
-const PORT = 3001;
-const MONGODB_URI = "mongodb://localhost:27017/my_local_db";
 
+const app = express();
+//const PORT = 3001;
+//const MONGODB_URI = "mongodb://localhost:27017/my_local_db";
+const MONGODB_URI = process.env.MONGODB_URI;
 app.use(cors());
 
 app.use(express.urlencoded({extended:true}));
@@ -18,14 +20,14 @@ app.use('/api/participant', require('./routes/participant'));
 app.use('/api/game', require('./routes/game'));
 app.use('/api/player', require('./routes/player'));
 
-// mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false });
 
-// mongoose.connection.once('open', function() {
-//     console.log('Connected to the Database');
-// });
-// mongoose.connection.on('error', function(error){
-//     console.log('Mongoose Connection Error : ' + error);
-// });
+mongoose.connection.once('open', function() {
+    console.log('Connected to the Database');
+});
+mongoose.connection.on('error', function(error){
+    console.log('Mongoose Connection Error : ' + error);
+});
 
 
 app.listen(process.env.PORT, function() {
